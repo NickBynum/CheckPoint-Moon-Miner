44af02fc881000fd3632ac2c638649acfb664143
Nick Bynum <exander@live.com>
2020-03-21 13:58:09 -0600

    added escavator - partial functionality

--- app.js
+++ app.js
@@ -32,19 +32,21 @@ function revealGame() {
   document.getElementById("section-game").classList.remove("hidden")
 }
 
+// Function to apply to modifiers and mine cheese
 function mineMoon() {
   cheese += 1 + (clickUpgrades.pickaxes.multiplier * clickUpgrades.pickaxes.quantity) + (clickUpgrades.jackHammers.multiplier * clickUpgrades.jackHammers.quantity)
   updateCheese()
 
 }
 
+//Ensure you have enough points to buy, and complete purchase
 function buyPickaxe() {
   if (cheese >= 20 + (clickUpgrades.pickaxes.quantity * 10)) {
     cheese -= 20 + (clickUpgrades.pickaxes.quantity * 10)
     clickUpgrades.pickaxes.quantity++
   }
   else {
-    console.log("You don't have enough for this purchase");
+    alert("You don't have enough for this purchase");
     
   }
   updateCheese()
@@ -57,7 +59,7 @@ function buyJackHammer() {
     clickUpgrades.jackHammers.quantity++
   }
   else {
-    console.log("You don't have enough for this purchase");
+    alert("You don't have enough for this purchase");
 
   }
   updateCheese()
@@ -88,6 +90,28 @@ function droidMine(){
   updateCheese()
 }
 
+function buyEscavator() {
+  if (automaticUpgrades.escavatorPit.inUse) {
+    return
+  }
+  else if (cheese >= automaticUpgrades.escavatorPit.price + (automaticUpgrades.escavatorPit.quantity * 10)) {
+    cheese -= automaticUpgrades.escavatorPit.price + (automaticUpgrades.escavatorPit.quantity * 10)
+    automaticUpgrades.escavatorPit.quantity++
+  }
+  else {
+    alert("You don't have enough for this purchase");
+
+  }
+  updateCheese()
+  updateInventory()
+  collectAutoUpgrades()
+}
+
+function escavatorMine(){
+  let result = cheese += 1 + (automaticUpgrades.escavatorPit.multiplier * automaticUpgrades.escavatorPit.quantity)
+  console.log("escavatorMine" + result)
+  updateCheese()
+}
 function updateCheese() {
   let cheeseCountElem = document.getElementById("cheese-count")
   cheeseCountElem.innerText = "Cheese Mined: " + cheese.toString()
@@ -104,9 +128,16 @@ function collectAutoUpgrades() {
 function updateInventory() {
   let pickaxeCountElem = document.getElementById("pickaxe-count")
   pickaxeCountElem.innerText = "Pickaxes Accumulated: " + clickUpgrades.pickaxes.quantity.toString()
-
+  
+  let jackHammerCountElem = document.getElementById("jackhammer-count")
+  jackHammerCountElem.innerText = "Jackhammers Accumulated: " + clickUpgrades.jackHammers.quantity.toString()
+  
   let droidCountElem = document.getElementById("droid-count")
   droidCountElem.innerText = "Droid Miners Accumulated: " + automaticUpgrades.droidMiners.quantity.toString()
+
+  let escavatorCountElem = document.getElementById("escavator-count")
+  escavatorCountElem.innerText = "Escavator Pits Created: " + automaticUpgrades.escavatorPit.quantity.toString()
+  updateStats()
 }
 
 function removeCoolDown(){
@@ -117,4 +148,22 @@ function removeCoolDown(){
 function applyCoolDown(){
   document.getElementById("buyDroidMinerbtn").classList.add("disabled")
   document.getElementById("buyDroidMinerbtn").disabled = true;
+}
+
+// STATS Functionality
+function updateStats() {
+let pickAxeMultElem = document.getElementById("pickaxe-mult")
+pickAxeMultElem.innerText = "Current Pickaxe Multiplier: " + (clickUpgrades.pickaxes.multiplier * clickUpgrades.pickaxes.quantity).toString()
+
+let jackhammerMultElem = document.getElementById("jackhammer-mult")
+jackhammerMultElem.innerText = "Current Jackhammer Multiplier: " + (clickUpgrades.jackHammers.multiplier * clickUpgrades.jackHammers.quantity).toString()
+
+let droidMultElem = document.getElementById("droid-mult")
+droidMultElem.innerText = "Current Droid Miners Multiplier: " + (automaticUpgrades.droidMiners.multiplier * automaticUpgrades.droidMiners.quantity).toString()
+
+let escavatorMultElem = document.getElementById("escavator-mult")
+escavatorMultElem.innerText = "Current Escavator Multiplier: " + (automaticUpgrades.escavatorPit.multiplier * automaticUpgrades.escavatorPit.quantity).toString()
+
+let totalMultElem = document.getElementById("total-mult")
+totalMultElem.innerText = "Total Multipliars being applied: " + (1 + (clickUpgrades.pickaxes.multiplier * clickUpgrades.pickaxes.quantity) + (clickUpgrades.jackHammers.multiplier * clickUpgrades.jackHammers.quantity) + (automaticUpgrades.droidMiners.multiplier * automaticUpgrades.droidMiners.quantity) + (automaticUpgrades.escavatorPit.multiplier * automaticUpgrades.escavatorPit.quantity)).toString()
 }
