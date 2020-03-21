7967070dfff87874da8ef780ae6494dd694282d1
Nick Bynum <exander@live.com>
2020-03-21 11:32:30 -0600

    Added Jackhammer and Droid timeouts

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ let clickUpgrades = {
     price: 100,
     quantity: 0,
     multiplier: 1
+  },
+
+  jackHammers: {
+    price: 10000,
+    quantity: 0,
+    multiplier: 10
   }
 };
 
@@ -11,7 +17,13 @@ let automaticUpgrades = {
   droidMiners: {
     price: 60,
     quantity: 0,
-    multiplier: 20
+    multiplier: 20,
+    inUse: false
+  },
+  escavatorPit: {
+    price: 100000,
+    quantity: 0,
+    multiplier: 1000
   }
 };
 
@@ -21,8 +33,9 @@ function revealGame() {
 }
 
 function mineMoon() {
-  cheese += 1 + (clickUpgrades.pickaxes.multiplier * clickUpgrades.pickaxes.quantity)
+  cheese += 1 + (clickUpgrades.pickaxes.multiplier * clickUpgrades.pickaxes.quantity) + (clickUpgrades.jackHammers.multiplier * clickUpgrades.jackHammers.quantity)
   updateCheese()
+
 }
 
 function buyPickaxe() {
@@ -32,16 +45,16 @@ function buyPickaxe() {
   }
   else {
     console.log("You don't have enough for this purchase");
-
+    
   }
   updateCheese()
   updateInventory()
 }
 
-function buyDroidMiner() {
-  if (cheese >= automaticUpgrades.droidMiners.price + (automaticUpgrades.droidMiners.quantity * 10)) {
-    cheese -= automaticUpgrades.droidMiners.price + (automaticUpgrades.droidMiners.quantity * 10)
-    automaticUpgrades.droidMiners.quantity++
+function buyJackHammer() {
+  if (cheese >= 100 + (clickUpgrades.jackHammers.quantity * 10)) {
+    cheese -= 100 + (clickUpgrades.jackHammers.quantity * 10)
+    clickUpgrades.jackHammers.quantity++
   }
   else {
     console.log("You don't have enough for this purchase");
@@ -49,19 +62,24 @@ function buyDroidMiner() {
   }
   updateCheese()
   updateInventory()
+}
+
+// This function completes the purchase of one droid and disables the ability to buy another
+function buyDroidMiner() {
+  if (automaticUpgrades.droidMiners.inUse) {
+    return
+  }
+  else if (cheese >= automaticUpgrades.droidMiners.price + (automaticUpgrades.droidMiners.quantity * 10)) {
+    cheese -= automaticUpgrades.droidMiners.price + (automaticUpgrades.droidMiners.quantity * 10)
+    automaticUpgrades.droidMiners.quantity++
+  }
+  else {
+    alert("You don't have enough for this purchase");
+
+  }
+  updateCheese()
+  updateInventory()
   collectAutoUpgrades()
-}
-
-function updateCheese() {
-  let cheeseCountElem = document.getElementById("cheese-count")
-  cheeseCountElem.innerText = "Cheese Mined: " + cheese.toString()
-}
-
-function collectAutoUpgrades() {
-  let interval = setInterval(droidMine, 3000)
-  setTimeout(function () { clearInterval(interval) }, 10000)
-  console.log("collectAutoUpgrades" + automaticUpgrades.droidMiners.multiplier);
-  
 }
 
 function droidMine(){
@@ -70,10 +88,33 @@ function droidMine(){
   updateCheese()
 }
 
+function updateCheese() {
+  let cheeseCountElem = document.getElementById("cheese-count")
+  cheeseCountElem.innerText = "Cheese Mined: " + cheese.toString()
+}
+
+function collectAutoUpgrades() {
+  applyCoolDown()
+  setTimeout(removeCoolDown, 10000)
+  let interval = setInterval(droidMine, 1000)
+  setTimeout(function () { clearInterval(interval) }, 10000)
+}
+
+
 function updateInventory() {
   let pickaxeCountElem = document.getElementById("pickaxe-count")
   pickaxeCountElem.innerText = "Pickaxes Accumulated: " + clickUpgrades.pickaxes.quantity.toString()
 
   let droidCountElem = document.getElementById("droid-count")
   droidCountElem.innerText = "Droid Miners Accumulated: " + automaticUpgrades.droidMiners.quantity.toString()
+}
+
+function removeCoolDown(){
+  document.getElementById("buyDroidMinerbtn").classList.remove("disabled")
+  document.getElementById("buyDroidMinerbtn").disabled = false;
+}
+
+function applyCoolDown(){
+  document.getElementById("buyDroidMinerbtn").classList.add("disabled")
+  document.getElementById("buyDroidMinerbtn").disabled = true;
 }
