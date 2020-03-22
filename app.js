ce19cced1955c842fb35e66cec7f8328b8ddaa77
Nick Bynum <exander@live.com>
2020-03-21 19:32:18 -0600

    Some refactoring done

--- app.js
+++ app.js
@@ -42,101 +42,138 @@ function mineMoon() {
 
 }
 
-//Ensure you have enough points to buy, and complete purchase
-function buyPickaxe() {
-  if (cheese >= 20 + (clickUpgrades.pickaxes.quantity * 10)) {
-    cheese -= 20 + (clickUpgrades.pickaxes.quantity * 10)
-    clickUpgrades.pickaxes.quantity++
+function buyClickUpgrades(clickChoice) {
+  let upgradePurch = clickUpgrades[clickChoice]
+  if (cheese >= upgradePurch.price + (upgradePurch.quantity * 10)) {
+    cheese -= upgradePurch.price + (upgradePurch.quantity * 10)
+    upgradePurch.quantity++
+    console.log("Item purchased")
   }
   else {
     alert("You don't have enough for this purchase");
-
   }
   updateCheese()
   updateInventory()
 }
 
-function buyJackHammer() {
-  if (cheese >= 100 + (clickUpgrades.jackHammers.quantity * 10)) {
-    cheese -= 100 + (clickUpgrades.jackHammers.quantity * 10)
-    clickUpgrades.jackHammers.quantity++
+// SECTION ******** replaced by refactoring *************
+// Ensure you have enough points to buy, and complete purchase
+// function buyPickaxe() {
+//   if (cheese >= 20 + (clickUpgrades.pickaxes.quantity * 10)) {
+//     cheese -= 20 + (clickUpgrades.pickaxes.quantity * 10)
+//     clickUpgrades.pickaxes.quantity++
+//   }
+//   else {
+//     alert("You don't have enough for this purchase");
+
+//   }
+//   updateCheese()
+//   updateInventory()
+// }
+
+// function buyJackHammer() {
+//   if (cheese >= 100 + (clickUpgrades.jackHammers.quantity * 10)) {
+//     cheese -= 100 + (clickUpgrades.jackHammers.quantity * 10)
+//     clickUpgrades.jackHammers.quantity++
+//   }
+//   else {
+//     alert("You don't have enough for this purchase");
+
+//   }
+//   updateCheese()
+//   updateInventory()
+// } 
+//#endregion
+
+function buyAutoUpgrades(clickChoice) {
+  let autoUpgradePurch = automaticUpgrades[clickChoice]
+  if (cheese >= autoUpgradePurch.price + (autoUpgradePurch.quantity * 10)) {
+    cheese -= autoUpgradePurch.price + (autoUpgradePurch.quantity * 10)
+    autoUpgradePurch.quantity++
+    console.log("Item purchased")
   }
   else {
     alert("You don't have enough for this purchase");
-
   }
   updateCheese()
   updateInventory()
 }
-//FIXME   buyDroidMiner() and buyEscavator() need consolidated into single function
+//SECTION *****REFACTOR COMPLETE*** 
+//  buyDroidMiner() and buyEscavator() need consolidated into single function
 // This function completes the purchase of one droid and disables the ability to buy another
-function buyDroidMiner() {
-  if (automaticUpgrades.droidMiners.inUse) {
-    return
-  }
-  else if (cheese >= automaticUpgrades.droidMiners.price + (automaticUpgrades.droidMiners.quantity * 10)) {
-    cheese -= automaticUpgrades.droidMiners.price + (automaticUpgrades.droidMiners.quantity * 10)
-    automaticUpgrades.droidMiners.quantity++
-  }
-  else {
-    alert("You don't have enough for this purchase");
-
-  }
+// function buyDroidMiner() {
+//   if (automaticUpgrades.droidMiners.inUse) {
+//     return
+//   }
+//   else if (cheese >= automaticUpgrades.droidMiners.price + (automaticUpgrades.droidMiners.quantity * 10)) {
+//     cheese -= automaticUpgrades.droidMiners.price + (automaticUpgrades.droidMiners.quantity * 10)
+//     automaticUpgrades.droidMiners.quantity++
+//   }
+//   else {
+//     alert("You don't have enough for this purchase");
+//   }
+//   updateCheese()
+//   updateInventory()
+// }
+// function buyEscavator() {
+//   if (automaticUpgrades.escavatorPit.inUse) {
+//     return
+//   }
+//   else if (cheese >= automaticUpgrades.escavatorPit.price + (automaticUpgrades.escavatorPit.quantity * 10)) {
+//     cheese -= automaticUpgrades.escavatorPit.price + (automaticUpgrades.escavatorPit.quantity * 10)
+//     automaticUpgrades.escavatorPit.quantity++
+//   }
+//   else {
+//     alert("You don't have enough for this purchase");
+//   }
+//   updateCheese()
+//   updateInventory()
+// }
+//#endregion
+// //SECTION ****refactored *****
+function autoMine(playerChoice) {
+  let autoUpgrade = automaticUpgrades[playerChoice]
+  let result = cheese += 1 + (autoUpgrade.multiplier * autoUpgrade.quantity)
   updateCheese()
-  updateInventory()
 }
 
-function droidMine() {
-  let result = cheese += 1 + (automaticUpgrades.droidMiners.multiplier * automaticUpgrades.droidMiners.quantity)
-  console.log("droidMine" + result)
-  collectAutoUpgrades()
-  updateCheese()
-}
 
-function buyEscavator() {
-  if (automaticUpgrades.escavatorPit.inUse) {
-    return
-  }
-  else if (cheese >= automaticUpgrades.escavatorPit.price + (automaticUpgrades.escavatorPit.quantity * 10)) {
-    cheese -= automaticUpgrades.escavatorPit.price + (automaticUpgrades.escavatorPit.quantity * 10)
-    automaticUpgrades.escavatorPit.quantity++
-  }
-  else {
-    alert("You don't have enough for this purchase");
+// function escavatorMine() {
+//   let result = cheese += 1 + (automaticUpgrades.escavatorPit.multiplier * automaticUpgrades.escavatorPit.quantity)
+//   updateCheese()
+// }
+////#endregion
 
-  }
-  updateCheese()
-  updateInventory()
-}
-
-function escavatorMine() {
-  let result = cheese += 1 + (automaticUpgrades.escavatorPit.multiplier * automaticUpgrades.escavatorPit.quantity)
-  updateCheese()
-
+// FIXME  escavatorMine() and droidMine() need consolidated into collectAutoUpgrades()
+function collectAutoUpgrades(playerChoice) {
+  let autoUpgrade = automaticUpgrades[playerChoice]
+  // if (autoUpgrade.inUse == true) {
+  //   return
+  // }
+  // applyCoolDown()
+  // setTimeout(removeCoolDown, 10000)
+  let interval = setInterval( autoMine(playerChoice), 1000)
+  setTimeout(function () { autoUpgrade.inUse = false; clearInterval(interval) }, 10000)
+  autoUpgrade.inUse = true
 }
 
 function updateCheese() {
   let cheeseCountElem = document.getElementById("cheese-count")
   cheeseCountElem.innerText = "Cheese Mined: " + cheese.toString()
 }
- // FIXME  escavatorMine() and droidMine() need consolidated into collectAutoUpgrades()
-function collectAutoUpgrades(playerChoice) {
-  let tool = automaticUpgrades[playerChoice]
-  applyCoolDown()
-  setTimeout(removeCoolDown, 10000)
-  let interval = setInterval(tool, 1000)
-  setTimeout(function () { clearInterval(interval) }, 10000)
-}
+
 
 // FIXME  Need to pull the correct autoupgrade that was selected to disable and renable the buttons
 function removeCoolDown() {
   document.getElementById("tool").classList.remove("disabled")
+  // @ts-ignore
   document.getElementById("tool").disabled = false;
 }
 
 function applyCoolDown(playerChoice) {
   let tool = automaticUpgrades[playerChoice]
   document.getElementById("tool").classList.add("disabled")
+  // @ts-ignore
   document.getElementById("tool").disabled = true;
 }
 
